8e2dc6ff2060a41d9f556b5d402e1b860390ddb9
TimofeyHub <timatofey@gmail.com>
2024-01-13 15:55:42 +0300

    add sold ad router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .scale_model.views import router as scale_model_router
+from .sold_ad.views import router as sold_ad_router
 
 router = APIRouter()
 router.include_router(router=scale_model_router, prefix="/scale-models")
+router.include_router(router=sold_ad_router, prefix="/sold_ad")
